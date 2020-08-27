c48a7b2129fa037728232c90e77cc8742c13158f
Dario <dariovp01@gmail.com>
2020-08-27 13:42:49 -0300

    Agregue instrucciones al Readme

--- client/src/main/java/donotforget/client/MainUI.java
+++ client/src/main/java/donotforget/client/MainUI.java
@@ -2,7 +2,6 @@ package donotforget.client;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -11,8 +10,6 @@ public class MainUI extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
        
         Scene scene = new Scene(new StackPane(), 640, 480);
         stage.setScene(scene);
