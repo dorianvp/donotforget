eb824bed84ca448e1287365b98549db6d02cf5be
dariovp <dariovp01@gmail.com>
2020-11-04 15:42:55 -0300

    Added css file to resources

--- client/src/main/java/donotforget/layout/MainView.java
+++ client/src/main/java/donotforget/layout/MainView.java
@@ -1,17 +1,26 @@
 package donotforget.layout;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text; 
 
 public class MainView extends VBox {
     public MainView() {
         super();
+        
         Text t = new Text("zurdooo");
         this.setMinHeight(200);
         this.setMinWidth(200);
 
         this.getChildren().add(t);
-        this.setStyle("-fx-background-color: brown;");
+
+        String css = this.getClass().getClassLoader().getResource("styles.css").toExternalForm();
+        this.getStylesheets().clear();
+        this.getStyleClass().add("root");
+        this.getStylesheets().add(css);
     }
 
 }
